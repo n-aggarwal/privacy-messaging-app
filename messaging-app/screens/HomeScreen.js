ca69144262e75b348a93367b5381d068834bfa4e
a-jacewicz <131491002+a-jacewicz@users.noreply.github.com>
2023-04-29 18:26:14 -0400

    got to point in making homescreen  where firebase is being used (believe that following this might have been issue with first time trying this)

--- messaging-app/screens/HomeScreen.js
+++ messaging-app/screens/HomeScreen.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import CustomListItem from "../components/CustomListItem";
+import { Avatar } from "react-native-elements";
 
 const HomeScreen = ({ navigation }) => {
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "PrivChat", 
+      headerStyle: { backgroundColor: "#fff" }, 
+      headerTitleStyle: { color: "black" },
+      headerTintColor: "black",  
+      headerLeft: () => <View style={{marginLeft : 20}}>
+      </View>
+    });  
+  }, []); 
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  );
+    <SafeAreaView>
+      <ScrollView>
+         <CustomListItem /> 
+      </ScrollView>
+    </SafeAreaView>
+   );
 };
 
 export default HomeScreen;
