247f47cd5eb2a318ab9ad9d8d408d1908c595266
Nishant <naggarwal@wesleyan.edu>
2023-04-30 14:38:05 -0400

    working frontend for chatScreen

--- messaging-app/screens/ChatScreen.js
+++ messaging-app/screens/ChatScreen.js
@@ -1,12 +1,11 @@
 import React, { useState, useLayoutEffect } from "react";
-import { TouchhableOpacity, TouchableWithoutFeedback } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
 import { Avatar } from "react-native-elements";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native";
 import { Platform } from "react-native";
-import { KeyboardAvoidingView } from "react-native";
 import { TextInput } from "react-native";
 import { ScrollView } from "react-native";
 import { firebase } from "../firebaseConfig";
@@ -40,12 +39,12 @@ const ChatScreen = ({ navigation, route }) => {
         </View>
       ),
       headerLeft: () => (
-        <TouchhableOpacity
+        <TouchableOpacity
           style={{ marginLeft: 10 }}
           onPress={navigation.goBack}
         >
           <AntDesign name="arrowleft" size={24} color="white" />
-        </TouchhableOpacity>
+        </TouchableOpacity>
       ),
       headerRight: () => (
         <View
@@ -56,19 +55,19 @@ const ChatScreen = ({ navigation, route }) => {
             marginRight: 20,
           }}
         >
-          <TouchhableOpacity>
+          <TouchableOpacity>
             <FontAwesome name="video-camera" size={24} color="white" />
-          </TouchhableOpacity>
-          <TouchhableOpacity>
-            <IonIcons name="call" size={24} color="white" />
-          </TouchhableOpacity>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name="call" size={24} color="white" />
+          </TouchableOpacity>
         </View>
       ),
     });
   }, [navigation, messages]);
 
   const sendMessage = () => {
-    keyboard.dismiss();
+    Keyboard.dismiss();
 
     firebase
       .firestore()
@@ -78,8 +77,8 @@ const ChatScreen = ({ navigation, route }) => {
       .add({
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         message: input,
-        displayName: auth.currentUser.displayName,
-        email: auth.currentUser,
+        displayName: firebase.auth().currentUser.displayName,
+        email: firebase.auth().currentUser,
         photoURL:
           "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
       });
@@ -118,7 +117,7 @@ const ChatScreen = ({ navigation, route }) => {
           <>
             <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
               {messages.map(({ id, data }) =>
-                data.email === auth.currentUser.email ? (
+                data.email === firebase.auth().currentUser.email ? (
                   <View key={id} style={styles.receiver}>
                     <Avatar
                       position="absolute"
@@ -133,7 +132,7 @@ const ChatScreen = ({ navigation, route }) => {
                       right={-5}
                       size={30}
                       source={{
-                        uri: data.photoURL,
+                        uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
                       }}
                     />
                     <Text style={styles.receiverText}>{data.message}</Text>
@@ -151,7 +150,7 @@ const ChatScreen = ({ navigation, route }) => {
                       left={-5}
                       size={30}
                       source={{
-                        uri: data.photoURL,
+                        uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
                       }}
                     />
                     <Text style={styles.senderText}>{data.message}</Text>
@@ -168,9 +167,9 @@ const ChatScreen = ({ navigation, route }) => {
                 placeholder="signal"
                 style={styles.textInput}
               />
-              <TouchhableOpacity onPress={sendMessage} activeOpacity={0.5}>
+              <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
                 <Ionicons name="send" size={24} color="#2B68E6" />
-              </TouchhableOpacity>
+              </TouchableOpacity>
             </View>
           </>
         </TouchableWithoutFeedback>
@@ -229,7 +228,7 @@ const styles = StyleSheet.create({
   },
   textInput: {
     bottom: 0,
-    haight: 40,
+    height: 40,
     flex: 1,
     marginRight: 15,
     backgroundColor: "#ECECEC",
