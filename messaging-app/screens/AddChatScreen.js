3e87b1cbebe5d0c037a550358f030c4f2a8a29d9
ryan kobayashi <rkobayashi@wesleyan.edu>
2023-04-29 22:09:53 -0400

    Completed the AddChatScreen up to the firebase set up. For full functionality, there are necessary changes to HomeScreen starting at about 2:07:11 in the video. I can do that as well once the HomeScreen and database are set up or someone else can pick that up.

--- messaging-app/screens/AddChatScreen.js
+++ messaging-app/screens/AddChatScreen.js
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { Button, Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+
+  const createChat = async () => {};
+
   return (
-    <View>
-      <Text>AddChatScreen</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        onSubmitEditing={createChat}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
+      <Button onPress={createChat} title="Create New Chat" />
     </View>
   );
 };
 
 export default AddChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 30,
+    height: "100%",
+  },
+});
